5254c6d0cdda70f8fce5596762daae6b586fdfc5
Rayan Wilbert <rayan.rmw@gmail.com>
2022-12-22 15:15:17 -0300

    Adding lombok on Kitchen Entity

--- src/main/java/com/api/algafood/domain/model/Kitchen.java
+++ src/main/java/com/api/algafood/domain/model/Kitchen.java
@@ -9,10 +9,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "kitchen")
 public class Kitchen {
 	
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,36 +31,4 @@ public class Kitchen {
 	@Column(name = "name")
 	private String name;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Kitchen other = (Kitchen) obj;
-		return Objects.equals(id, other.id);
-	}
 }
