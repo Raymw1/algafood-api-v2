03cb74c91d10b1e9d770c6729bbe61e8c35be76e
Rayan Wilbert <rayan.rmw@gmail.com>
2022-12-23 11:44:31 -0300

    Entity Mapping Challenge 3.20: State

---
+++ src/main/java/com/api/algafood/domain/repository/StateRepository.java
@@ -0,0 +1,15 @@
+package com.api.algafood.domain.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.api.algafood.domain.model.State;
+
+@Repository
+public interface StateRepository {
+	List<State> findAll();
+	State findOne(Long id);
+	State save(State state);
+	State remove(State state);
+}
