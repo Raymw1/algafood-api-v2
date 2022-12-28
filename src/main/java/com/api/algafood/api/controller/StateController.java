fc66d4a3b1031cb287cbd408f4de7b03e1a662a0
Rayan <rayan.rmw@gmail.com>
2022-12-28 14:53:10 -0300

    Requesting Singleton Resource

--- src/main/java/com/api/algafood/api/controller/StateController.java
+++ src/main/java/com/api/algafood/api/controller/StateController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class StateController {
 	@GetMapping
 	public List<State> list() {
 		return stateRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public State findOne(@PathVariable Long id) {
+		return stateRepository.findOne(id);
 	}
 	
 }
