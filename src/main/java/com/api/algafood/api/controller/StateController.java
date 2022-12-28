ccc307e014abf1b1c0a3f992a511c7dfa31fb49b
Rayan <rayan.rmw@gmail.com>
2022-12-28 14:29:33 -0300

    Add states list endpoint

---
+++ src/main/java/com/api/algafood/api/controller/StateController.java
@@ -0,0 +1,25 @@
+package com.api.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.algafood.domain.model.State;
+import com.api.algafood.domain.repository.StateRepository;
+
+@RestController
+@RequestMapping("/states")
+public class StateController {
+
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@GetMapping
+	public List<State> list() {
+		return stateRepository.findAll();
+	}
+	
+}
