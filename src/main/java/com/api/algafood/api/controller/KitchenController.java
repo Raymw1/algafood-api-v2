acadd2c9d06b8f311db15185098ee180841125a3
Rayan <rayan.rmw@gmail.com>
2022-12-29 15:25:10 -0300

    Set response status with @ResponseStatus

--- src/main/java/com/api/algafood/api/controller/KitchenController.java
+++ src/main/java/com/api/algafood/api/controller/KitchenController.java
@@ -3,9 +3,12 @@ package com.api.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.algafood.api.model.KitchensXmlWrapper;
@@ -29,5 +32,11 @@ public class KitchenController {
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public KitchensXmlWrapper listXml() {
 		return new KitchensXmlWrapper(kitchenRepository.findAll());
+	}
+	
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping("/{id}")
+	public Kitchen findOne(@PathVariable Long id) {
+		return kitchenRepository.findOne(id);
 	}
 }
