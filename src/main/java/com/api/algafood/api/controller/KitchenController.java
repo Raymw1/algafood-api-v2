a5751346ee4ae65ae7059d0db3c9fb7aefbc54ab
Rayan Wilbert <rayan.rmw@gmail.com>
2023-01-02 11:48:50 -0300

    Add POST method to kitchen resource

--- src/main/java/com/api/algafood/api/controller/KitchenController.java
+++ src/main/java/com/api/algafood/api/controller/KitchenController.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,5 +56,12 @@ public class KitchenController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		return ResponseEntity.ok(kitchen);
+	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping
+	public Kitchen save(@RequestBody Kitchen kitchen) {
+		kitchen = kitchenRepository.save(kitchen);
+		return kitchen;
 	}
 }
