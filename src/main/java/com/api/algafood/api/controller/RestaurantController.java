c262732d2195312477730e2d80f58a2779405673
Rayan Wilbert <rayan.rmw@gmail.com>
2023-01-02 14:46:50 -0300

    Add POST methods for restaurant resource

--- src/main/java/com/api/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/api/algafood/api/controller/RestaurantController.java
@@ -2,10 +2,15 @@ package com.api.algafood.api.controller;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +38,13 @@ public class RestaurantController {
 		return ResponseEntity.ok(restaurant);
 	}
 	
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Restaurant restaurant) {
+		try {
+			restaurant = restaurantService.save(restaurant);
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
 }
--- src/main/java/com/api/algafood/domain/service/RestaurantService.java
+++ src/main/java/com/api/algafood/domain/service/RestaurantService.java
@@ -2,10 +2,14 @@ package com.api.algafood.domain.service;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.algafood.domain.model.Kitchen;
 import com.api.algafood.domain.model.Restaurant;
+import com.api.algafood.domain.repository.KitchenRepository;
 import com.api.algafood.domain.repository.RestaurantRepository;
 
 @Service
@@ -14,12 +18,27 @@ public class RestaurantService {
 	@Autowired
 	private RestaurantRepository restaurantRepository;
 	
+	@Autowired
+	private KitchenRepository kitchenRepository;
+	
 	public List<Restaurant> findAll() {
 		return restaurantRepository.findAll();
 	}
 	
 	public Restaurant findOne(Long id) {
 		return restaurantRepository.findOne(id);
+	}
+	
+	public Restaurant save(Restaurant restaurant) {
+		Long kitchenId = restaurant.getKitchen().getId();
+		Kitchen kitchen = kitchenRepository.findOne(kitchenId);
+		if (kitchen == null) {
+			throw new EntityNotFoundException(
+				String.format("Kitchen with id %d does not exist!", kitchenId)
+			);
+		}
+		restaurant.setKitchen(kitchen);
+		return restaurantRepository.save(restaurant);
 	}
 
 }
